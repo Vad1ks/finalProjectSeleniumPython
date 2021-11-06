15e561ec449870a20f1d0863655476f713ae242a
VadymS <vadiksosnovenko@gmail.com>
2021-11-06 17:38:55 +0200

    Refactored test to make it work with different content(languages)

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,12 +9,23 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
 
     def should_be_correct_success_message(self):
-        expected_success_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text \
-     + " has been added to your basket."
-        actual_success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
-        assert expected_success_message == actual_success_message, "Success message is incorrect"
+        product_name = self.get_product_name()
+        actual_success_message = self.get_success_message()
+        assert product_name in actual_success_message, "Success message is incorrect"
 
     def should_be_correct_basket_price(self):
-        actual_basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
-        expected_basket_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        actual_basket_price = self.get_basket_price()
+        expected_basket_price = self.get_product_price()
         assert actual_basket_price == expected_basket_price, "The  basket price is incorrect"
+
+    def get_success_message(self):
+        return self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_basket_price(self):
+        return self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+    
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
