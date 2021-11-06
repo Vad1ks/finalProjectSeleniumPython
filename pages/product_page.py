4abc588e228c1644da90d7fcae1e684c55b40c54
VadymS <vadiksosnovenko@gmail.com>
2021-11-06 20:33:11 +0200

    added xfail tests, moved mainpage methods to base class

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,7 +5,7 @@ class ProductPage(BasePage):
 
     def add_product_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
-        self.solve_quiz_and_get_code()
+        #self.solve_quiz_and_get_code()
 
     def should_be_correct_success_message(self):
         product_name = self.get_product_name()
@@ -16,6 +16,14 @@ class ProductPage(BasePage):
         actual_basket_price = self.get_basket_price()
         expected_basket_price = self.get_product_price()
         assert actual_basket_price == expected_basket_price, "The  basket price is incorrect"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should have disappered"
 
     def get_success_message(self):
         return self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,47 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
-@pytest.mark.parametrize('promo_num', [1,2,3,4,5,6,7,8,9])
-def test_guest_can_add_product_to_basket(browser,promo_num):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_num}"
+# @pytest.mark.parametrize('promo_num', [1,2,3,4,5,6,pytest.param(7, marks=pytest.mark.xfail(reason="bug")),8,9])
+# def test_guest_can_add_product_to_basket(browser,promo_num):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_num}"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_product_to_basket()
+#     page.should_be_correct_success_message()
+#     page.should_be_correct_basket_price()
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    page.should_be_correct_success_message()
-    page.should_be_correct_basket_price()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_disappear_success_message()
